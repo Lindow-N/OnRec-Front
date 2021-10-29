f914d5d55b8cafdebaada584815e44c82bb05e02
Lindow-N <anthonycasaresit@gmail.com>
2021-10-29 17:03:57 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter,Route} from 'react-router-dom';
+import ScrollToTop from './containers/Router/GoTop';
+import useAdmin from'./containers/Router/Admin';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Accueil from "../src/components/Accueil/Accueil";
+import PrestationsTarifs from "../src/components/PrestationsTarifs/PrestationsTarifs";
+import Présentation from "../src/components/Présentation/Présentation";
+import Réalisation from "../src/components/Réalisation/Réalisation";
+import Contact from "../src/components/Contact/Contact";
+import Identification from "../src/components/Admin/Identification/Identification";
+import Admin from "../src/components/Admin/Admin/Admin";
+import PrivateRoute from './containers/Router/Admin'; 
 
-export default App;
+function App () {
+ 
+
+  const isAuthenticated = localStorage.getItem('admin');;
+
+
+    return (
+    
+      <BrowserRouter>
+      <ScrollToTop>
+      <Route exact  path="/" component={Accueil}></Route>
+      <Route exact  path="/PrestationsTarifs" component={PrestationsTarifs}></Route>
+      <Route exact  path="/Présentation" component={Présentation}></Route>
+      <Route exact  path="/Réalisation" component={Réalisation}></Route>
+      <Route exact  path="/Contact" component={Contact}></Route>
+      <Route exact  path="/Admin" component={Identification}></Route>
+      <PrivateRoute exact  path="/Admin/Réalisation" component={Admin} isAuthenticated={isAuthenticated} ></PrivateRoute>
+      </ScrollToTop>
+      
+      </BrowserRouter>
+     
+    
+    )
+  }
+
+
+export default App
