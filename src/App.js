88e846088a26eb0353966cb520d8af37b2d5546d
Lindow-N <anthonycasaresit@gmail.com>
2021-11-16 15:37:25 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter,Route} from 'react-router-dom';
 import ScrollToTop from './containers/Router/GoTop';
 import useAdmin from'./containers/Router/Admin';
 
+
 import Accueil from "../src/components/Accueil/Accueil";
 import PrestationsTarifs from "../src/components/PrestationsTarifs/PrestationsTarifs";
 import Présentation from "../src/components/Présentation/Présentation";
@@ -12,21 +13,24 @@ import Identification from "../src/components/Admin/Identification/Identificatio
 import Admin from "../src/components/Admin/Admin/Admin";
 import PrivateRoute from './containers/Router/Admin'; 
 
+
+
+
 function App () {
  
 
-  const isAuthenticated = localStorage.getItem('admin');;
+  const isAuthenticated = localStorage.getItem('admin');
+ 
 
 
     return (
     
+      
       <BrowserRouter>
       <ScrollToTop>
       <Route exact  path="/" component={Accueil}></Route>
       <Route exact  path="/PrestationsTarifs" component={PrestationsTarifs}></Route>
-      <Route exact  path="/Présentation" component={Présentation}></Route>
-      <Route exact  path="/Réalisation" component={Réalisation}></Route>
-      <Route exact  path="/Contact" component={Contact}></Route>
+      <Route exact  path="/Présentation" component={Présentation}></Route>      <Route exact  path="/Contact" component={Contact}></Route>
       <Route exact  path="/Admin" component={Identification}></Route>
       <PrivateRoute exact  path="/Admin/Réalisation" component={Admin} isAuthenticated={isAuthenticated} ></PrivateRoute>
       </ScrollToTop>
